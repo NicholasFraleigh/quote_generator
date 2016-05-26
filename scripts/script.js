7ba15596bf9d004a7929857b6935dd771eae5525
Nicholas Fraleigh <nicholasfraleigh@gmail.com>
2016-05-26 11:10:23 -0700

    format display nicely with different colours

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,10 @@
 // function to get jSon file
 $(document).ready(function(){
 	fetchJson();
+	setRandomColours();
 	$("#getMessage").click(function(){
 		fetchJson();
+		setRandomColours();
 	})
 });
          
@@ -16,12 +18,33 @@ function fetchJson(){
     		document.getElementById("quote").innerHTML = '"' + data['quote'] + '"';
     		document.getElementById('author').innerHTML = '~ ' + data['author'];
     		checkObj(data);
+    		$('#twitter').attr('href', 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + data['quote'] + '" ~ ' + data['author']));
     	},
     	error: function(err) { alert(err); },
     	beforeSend: function(xhr) {
     	xhr.setRequestHeader("X-Mashape-Authorization", "V8ZOzPisBbmshdMDf7IJypZjSb6ap14hXKHjsnqLmKz2cMJcHM"); // Enter here your Mashape key
     }
 });
+}
+
+function setRandomColours(){
+	var colour1 = '#'; 
+	var colour2 = '#';
+	// set up arrays such that each element goes well with element of same number in other array
+    var letters1 = ['FF7575', 'FF79E1', 'FF73B9', 'FE67EB', 'E77AFE', 'D97BFD', 'A27AFE']; 
+    var letters2 = ['FFDFDF', 'FFDFF8', 'FFDFEF', 'FFDBFB', 'F9D9FF', 'F4DCFE', 'E6DBFF']; 
+    var selector = Math.floor(Math.random() * letters1.length);
+    colour1 += letters1[selector];
+    colour2 += letters2[selector];
+    document.getElementById('quote-box').style.background = colour2; // Setting the random color on div element.
+    document.getElementById('outer-box').style.background = colour1;
+    document.getElementById('quote').style.color = colour1;
+    document.getElementById('author').style.color = colour1;
+    document.getElementById('bod').style.background = colour1; // Setting the random color on div element.
+    document.getElementById('getMessage').style.color = colour1;
+    document.getElementById('getMessage').style.background = colour2;
+    document.getElementById('twitter').style.color = colour2;
+    document.getElementById('facebook').style.color = colour2;
 }
 
 
